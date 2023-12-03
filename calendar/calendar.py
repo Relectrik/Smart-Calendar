6f4f66b22b497d645136be30929208f4356fb7bf
Relectrik <viv@dluvs.com>
2023-12-02 21:26:41 -0800

    Added classes for constraints.

--- calendar/calendar.py
+++ calendar/calendar.py
@@ -1,4 +1,111 @@
 from datetime import timedelta
+import copy
+
+
+class DateConstraint:
+    def __init__(self, l_val, operator, arity):
+        self.legal_ops = {"==", "!=", "<", "<=", ">", ">="}
+
+        if operator not in self.legal_ops:
+            raise Exception("Invalid constraint operator")
+        if l_val < 0:
+            raise Exception("Invalid variable index")
+        self.l_val = l_val
+        self.op = operator
+        self.arity = arity
+
+    def is_satisfied_by(self, leftDate, rightDate):
+        match self.op:
+            case "==":
+                return leftDate == rightDate
+            case "!=":
+                return leftDate != rightDate
+            case ">":
+                return leftDate > rightDate
+            case "<":
+                return leftDate < rightDate
+            case ">=":
+                return leftDate >= rightDate
+            case "<=":
+                return leftDate <= rightDate
+        return False
+
+    def get_symmetrical_op(self):
+        match self.op:
+            case ">":
+                return "<"
+            case "<":
+                return ">"
+            case ">=":
+                return "<="
+            case "<=":
+                return ">-"
+            case _:
+                return self.op
+
+    def arity(self):
+        return self.arity
+
+    def __str__(self) -> str:
+        return self.l_val + " " + self.op
+
+
+class UnaryConstraint(DateConstraint):
+    def __init__(self, l_val, operator, r_val):
+        super().__init__(l_val=l_val, operator=operator, arity=1)
+        self.r_val = r_val
+
+    def __str__(self) -> str:
+        return super().__str__() + " " + self.r_val
+
+    def __eq__(self, other) -> bool:
+        if self == other:
+            return True
+        if type(self) != type(other):
+            return False
+        otherDC = copy.deepcopy(other)
+        return (
+            self.l_val == otherDC.l_val
+            and self.op == otherDC.op
+            and self.r_val == otherDC.r_val
+        )
+
+    def __hash__(self) -> int:
+        return hash(self.l_val) * hash(self.op) * hash(self.r_val)
+
+
+class BinaryConstraint(DateConstraint):
+    def __init__(self, l_val, operator, r_val):
+        super().__init__(l_val=l_val, operator=operator, arity=2)
+        if r_val < 0 or l_val == r_val:
+            raise Exception("Invalid variable Index")
+        self.r_val = r_val
+
+    def getReverse(self):
+        return BinaryConstraint(self.r_val, self.get_symmetrical_op(), self.l_val)
+
+    def __eq__(self, other) -> bool:
+        if self == other:
+            return True
+        if type(self) != type(other):
+            return False
+        otherDC = copy.deepcopy(other)
+        reversed = self.getReverse()
+        return (
+            self.l_val == other.l_val
+            and self.op == otherDC.op
+            and self.r_val == otherDC.r_val
+        ) or (
+            reversed.l_val == otherDC.l_val
+            and reversed.op == otherDC.op
+            and reversed.r_val == otherDC.r_val
+        )
+
+    def __hash__(self) -> int:
+        return hash(self.l_val) * hash(self.op) * hash(self.r_val)
+
+    def __str__(self) -> str:
+        return super().__str__() + " " + self.r_val
 
 
 class Event:
