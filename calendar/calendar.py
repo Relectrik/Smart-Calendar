69c7b0e97a3b7c1c844fe0e33e3c46e87a800f0a
Relectrik <viv@dluvs.com>
2023-12-06 19:02:56 -0800

    Finished translating old Java code to Python.

--- calendar/calendar.py
+++ calendar/calendar.py
@@ -3,6 +3,7 @@ import copy
 from datetime import datetime
 
 
+# Objects & Classes for CSP Solver
 class DateConstraint:
     def __init__(self, l_val, operator, arity):
         self.legal_ops = {"==", "!=", "<", "<=", ">", ">="}
@@ -82,7 +83,7 @@ class BinaryConstraint(DateConstraint):
             raise Exception("Invalid variable Index")
         self.r_val = r_val
 
-    def getReverse(self):
+    def get_reverse(self):
         return BinaryConstraint(self.r_val, self.get_symmetrical_op(), self.l_val)
 
     def __eq__(self, other) -> bool:
@@ -124,6 +125,30 @@ class MeetingDomain:
         return f"{self.domain_values}"
 
 
+class Arc:
+    def __init__(self, tail, head, constraint):
+        self.tail = tail
+        self.head = head
+        self.constraint = constraint
+
+    def __eq__(self, other) -> bool:
+        if self == other:
+            return True
+        if type(self) != type(other):
+            return False
+        other_arc = copy.deepcopy(other)
+        return (
+            self.tail
+            == other_arc.tail & self.head
+            == other_arc.head & self.constraint
+            == other_arc.constraint
+        )
+
+    def __str__(self) -> str:
+        return f"({self.tail} -> {self.head})"
+
+
+# Calendar
 class Event:
     def __init__(
         self,
@@ -163,6 +188,55 @@ class Calendar:
                     return
                     # do smth
 
+    def solve(self, n_meetings, range_start, range_end, constraints):
+        index_domain = self.meeting_domain_list(n_meetings, range_start, range_end)
+        self.node_consistency(index_domain, constraints)
+        self.arc_consistency(index_domain, constraints)
+        return self.solve_recursively(n_meetings, constraints, [], index_domain)
+
+    def solve_recursively(self, n_meetings, constraints, assignments, index_domains):
+        if len(assignments) == n_meetings:
+            return assignments
+        for domain in index_domains:
+            for date in domain.domain_values:
+                if self.is_consistent(date, constraints, assignments):
+                    assignments.add(date)
+                    results = self.solve_recursively(
+                        n_meetings, constraints, assignments, index_domains
+                    )
+                    if results != None:
+                        return results
+                    assignments.pop(len(assignments) - 1)
+        return None
+
+    def is_consistent(date, constraints, assignments):
+        assignments.add(date)
+        for constraint in constraints:
+            if constraint.arity == 1:
+                try:
+                    assignments.get(constraint.l_val)
+                except IndexError:
+                    continue
+                if constraint.is_satisfied_by(constraint.l_val, constraint.r_val):
+                    continue
+                else:
+                    assignments.pop(len(assignments) - 1)
+                    return False
+
+            else:
+                try:
+                    assignments.get(constraint.l_val)
+                    assignments.get(constraint.r_val)
+                except IndexError:
+                    continue
+                if constraint.is_satisfied_by(constraint.l_val, constraint.r_val):
+                    continue
+                else:
+                    assignments.pop(len(assignments) - 1)
+
+        assignments.pop(len(assignments) - 1)
+        return True
+
     def find_optimal_time_slot(self):
         # [!] TODO:
         # Must find best timeslots for every type of task in pre-existing calendar.
@@ -190,13 +264,56 @@ class Calendar:
     def priority_add(self):
         return
 
-    def node_consistency():
-        return
+    def node_consistency(self, var_domains, constraints):
+        for constraint in constraints:
+            if constraint.arity() == 1:
+                index_domain = var_domains[constraint.l_val]
+                index_domain_copy = copy.deepcopy(index_domain)
+                for date in index_domain_copy.domain_values:
+                    if not constraint.is_satisfied_by(date, constraint.l_val):
+                        index_domain.domain_values.remove(date)
 
-    def arc_consistency():
-        return
+    def arc_consistency(self, var_domains, constraints):
+        arc_queue = set()
+        for constraint in constraints:
+            if constraint.arity() == 2:
+                arc_queue.add(Arc(constraint.l_val, constraint.r_val, constraint))
+                arc_queue.add(
+                    Arc(constraint.r_val, constraint.l_val, constraint.get_reverse())
+                )
+        self.empty_arc_queue(arc_queue, var_domains, constraints)
 
+    def empty_arc_queue(self, arc_queue, var_domains, constraints):
+        while arc_queue:
+            polled_arc = next(iter(arc_queue))
+            arc_queue.remove(polled_arc)
+            if self.remove_inconsistent(polled_arc, var_domains):
+                for constraint in constraints:
+                    if constraint.arity() == 2:
+                        if constraint.l_val == polled_arc.tail:
+                            arc_queue.add(
+                                Arc(
+                                    constraint.l_val,
+                                    constraint.r_val,
+                                    constraint.get_reverse(),
+                                )
+                            )
+                        if constraint.r_val == polled_arc.tail:
+                            arc_queue.add(
+                                Arc(constraint.l_val, constraint.r_val, constraint)
+                            )
 
-# [!] To-do next time:
-# Mypy
-# Take a crack at some heuristic if possible
+    def remove_inconsistent(self, arc, var_domains):
+        removed = False
+        count = 0
+        tail_domain = var_domains.get(arc.tail)
+        tail_domain_copy = copy.deepcopy(tail_domain)
+        for tail_date in tail_domain_copy.domain_values:
+            for head_date in var_domains.get(arc.head).domain_values:
+                if not arc.constraint.is_satisfied_by(tail_date, head_date):
+                    count += 1
+            if count == len(var_domains.get(arc.head).domain_values):
+                tail_domain.domain_values.remove(tail_date)
+                removed = True
+            count = 0
+        return removed
